ce2434f5c05e781d23223866f96e1a5687e86d92
vuun <sown.squak@gmail.com>
2015-10-04 18:10:31 +0700

    Fin UI Desc no inside func yet, Fin Search UI no inside func also

---
+++ Description/search/src/main/java/com/example/vuun/search/ExpandListChild.java
@@ -0,0 +1,24 @@
+package com.example.vuun.search;
+
+/**
+ * Created by vuun on 10/4/2015.
+ */
+
+public class ExpandListChild {
+
+    private String Name;
+    private String Tag;
+
+    public String getName() {
+        return Name;
+    }
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+    public String getTag() {
+        return Tag;
+    }
+    public void setTag(String Tag) {
+        this.Tag = Tag;
+    }
+}
